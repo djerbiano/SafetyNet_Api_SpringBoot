0356eeb78e0682d0a9f24ea118ac51ef32f91871
Saber Ghoudi <saberghoudi2222@hotmail.fr>
2026-06-07 01:06:22 +0200

    ajout du DTO PersonDetailDTO

---
+++ src/main/java/com/safetynet/dto/PersonDetailDTO.java
@@ -0,0 +1,17 @@
+package com.safetynet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class PersonDetailDTO {
+    private String firstName;
+    private String address;
+    private int age;
+    private String email;
+    private List<String> medications;
+    private List<String> allergies;
+}
